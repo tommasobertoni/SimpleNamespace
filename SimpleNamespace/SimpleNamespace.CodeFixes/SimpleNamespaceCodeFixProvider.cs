ff07ddb42dedacc0206d8e40752f7fe22f3e456e
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2021-01-29 10:08:11 +0100

    Adds analyzer for public types in non-simple namespaces.

---
+++ SimpleNamespace/SimpleNamespace.CodeFixes/SimpleNamespaceCodeFixProvider.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SimpleNamespace
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(SimpleNamespaceCodeFixProvider)), Shared]
+    public class SimpleNamespaceCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds { get; } =
+            ImmutableArray.Create(SimpleNamespaceAnalyzer.DiagnosticId);
+
+        public sealed override FixAllProvider? GetFixAllProvider() => null;
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var action = CodeAction.Create(
+                title: CodeFixResources.CodeFixTitle,
+                createChangedDocument: _ => MakeSimpleNamespace(root, context),
+                equivalenceKey: nameof(CodeFixResources.CodeFixTitle));
+
+            var diagnostic = context.Diagnostics.First();
+
+            context.RegisterCodeFix(action, diagnostic);
+        }
+
+        private Task<Document> MakeSimpleNamespace(
+            SyntaxNode root,
+            CodeFixContext context)
+        {
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var namespaceDecl = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<NamespaceDeclarationSyntax>().First();
+
+            // Compute new simple name.
+            var @namespace = namespaceDecl.Name.ToString();
+            var namespaceTokens = @namespace.Split('.');
+            var newName = namespaceTokens[0];
+
+            // Replace namespace declaration.
+            var newNamespaceDecl = namespaceDecl.WithName(SyntaxFactory.IdentifierName(newName));
+            var newRoot = root.ReplaceNode(namespaceDecl, newNamespaceDecl);
+
+            var document = context.Document;
+            var newDocument = document.WithSyntaxRoot(newRoot);
+
+            return Task.FromResult(newDocument);
+        }
+    }
+}
---
+++ SimpleNamespace/SimpleNamespace/SimpleNamespaceAnalyzer.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace SimpleNamespace
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SimpleNamespaceAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "SimpleNamespace";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(
+            nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(
+            nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+
+        private static readonly LocalizableString Description = new LocalizableResourceString(
+            nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId,
+            Title,
+            MessageFormat,
+            Category,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSymbolAction(AnalyzeNamespace, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeNamespace(SymbolAnalysisContext context)
+        {
+            var typeSymbol = (INamedTypeSymbol)context.Symbol;
+
+            if (!IsPublic(typeSymbol)) return;
+            if (HasParentType(typeSymbol)) return;
+            if (HasEasyNamespace(typeSymbol)) return;
+            if (HasParentNamespace(typeSymbol)) return;
+
+            var @namespace = typeSymbol.ContainingNamespace.ToString();
+            var diagnosticLocation = typeSymbol.Locations[0];
+            var diagnostic = Diagnostic.Create(Rule, diagnosticLocation, @namespace, typeSymbol.Name);
+
+            context.ReportDiagnostic(diagnostic);
+        }
+
+        private static bool IsPublic(INamedTypeSymbol typeSymbol) =>
+            typeSymbol.DeclaredAccessibility == Accessibility.Public ||
+            typeSymbol.DeclaredAccessibility == Accessibility.Protected;
+
+        private static bool HasParentType(INamedTypeSymbol typeSymbol) =>
+            typeSymbol.ContainingType is not null;
+
+        private static bool HasEasyNamespace(INamedTypeSymbol typeSymbol)
+        {
+            var namespaceSymbol = typeSymbol.ContainingNamespace;
+            if (namespaceSymbol.IsGlobalNamespace) return true;
+
+            var @namespace = namespaceSymbol.OriginalDefinition.ToString();
+            return @namespace.IndexOf('.') == -1;
+        }
+
+        private static bool HasParentNamespace(INamedTypeSymbol typeSymbol)
+        {
+            var namespaceSymbol = typeSymbol.ContainingNamespace;
+            var parentNamespaceSymbol = namespaceSymbol.ContainingNamespace;
+
+            if (parentNamespaceSymbol is null) return false;
+
+            // A namespace can be contained within another namespace in two ways:
+            // as a dot-separated namespace:
+            //   namespace Foo.Bar
+            //   {
+            //   }
+            // 
+            // as an explicit nested declaration:
+            //   namespace Foo
+            //   {
+            //       namespace Bar
+            //       {
+            //       }
+            //   }
+
+            return IsNestedIn(child: namespaceSymbol, parent: parentNamespaceSymbol);
+        }
+
+        private static bool IsNestedIn(INamespaceSymbol child, INamespaceSymbol parent)
+        {
+            var childStartPosition = child.Locations[0].SourceSpan.Start;
+            var parentEndPosition = parent.Locations[0].SourceSpan.End;
+            return parentEndPosition + 1 /* the dot separating the tokens */ != childStartPosition;
+        }
+    }
+}
